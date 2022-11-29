b704a7af41e03f1925c583743ba93dd2f1fa5a00
connectriaj <connect.riaj@hotmail.com>
2022-11-29 18:57:57 +0600

    user api created successfully

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,8 +19,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const serviceCollection = client.db("ultraNet").collection("services");
-    const allServiceCollection = client.db("ultraNet").collection("allServices");
-
+    const allServiceCollection = client
+      .db("ultraNet")
+      .collection("allServices");
+    const orderCollection = client.db("ultraNet").collection("orders");
 
     app.get("/services", async (req, res) => {
       const query = {};
@@ -34,6 +36,20 @@ async function run() {
       const cursor = allServiceCollection.find(query);
       const allServices = await cursor.toArray();
       res.send(allServices);
+    });
+
+    app.get("/all-services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await allServiceCollection.findOne(query);
+      res.send(service);
+    });
+
+    // orders api start from here
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
     });
   } finally {
     // nothing to do here
